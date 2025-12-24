fbe5a13bdcf8c7c7cca3d9a980d14872e914b45d
Manish Jadaun <manish.jadaun2606@gmail.com>
2025-12-24 06:30:36 +0530

    Add delete task feature

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ function addTask() {
 
     let li = document.createElement("li");
     li.textContent = task;
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
 
     let list = document.getElementById("taskList");
     list.appendChild(li);
